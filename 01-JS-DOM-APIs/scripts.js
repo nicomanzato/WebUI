2331502798746fb98e4f0bbdfaa41ecf698b3813
nicomanzato <nicomanzato@gmail.com>
2018-10-16 15:01:31 -0300

    2. DONE

--- 01-JS-DOM-APIs/scripts.js
+++ 01-JS-DOM-APIs/scripts.js
@@ -2,3 +2,7 @@ function fadeIn() {
   let hiddenSection = document.getElementById("hiddenSection");
   hiddenSection.style.display  = "block";
 }
+
+function invokeAlert() {
+  alert("run like hell !!");
+}

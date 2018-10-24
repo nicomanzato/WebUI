e8b2136be29f20ce8c42dc0f4c3592a8685565c7
nicomanzato <nicomanzato@gmail.com>
2018-10-24 11:39:37 -0300

    webpack integrated with babel

--- 02-OOP-inheritance/webpack.config.js
+++ 02-OOP-inheritance/webpack.config.js
@@ -1,10 +1,21 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './js/playground.js',
   output: {
     path: path.resolve(__dirname),
     filename: 'bundle.js'
+  },
+  module: {
+         rules: [
+             {
+                 test: /\.js$/,
+                 loader: 'babel-loader',
+                 options: {
+                    presets: ['@babel/preset-env']
+                 }
+             }
+         ]
   }
 };

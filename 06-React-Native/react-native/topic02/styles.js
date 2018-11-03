21b55c9a4b18ef2fbc062fb32c97890c2fd11803
nicomanzato <nicomanzato@gmail.com>
2018-11-03 15:18:10 -0300

    topic02: 6. added

--- 06-React-Native/react-native/topic02/styles.js
+++ 06-React-Native/react-native/topic02/styles.js
@@ -1,14 +1,29 @@
-import { StyleSheet, Text, View, ScrollView, Image, Dimensions} from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, Dimensions, Platform} from 'react-native';
 
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'skyblue',
+    backgroundColor: 'white',
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'space-between'
+  },
+  article: {
+    borderWidth: 1,
+    borderColor: 'black',
+    flexDirection: 'row',
+  },
+  articleImg: {
+    flex: 1,
+    alignSelf: 'center'
+  },
+  content: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20
   },
   box: {
     margin: 10,
@@ -19,6 +34,14 @@ const styles = StyleSheet.create({
     height: 100,
     width: 100,
     alignSelf: 'flex-start'
+  },
+  socialNetworkImage: {
+    width: 50,
+    height: 50,
+    margin: 5
+  },
+  socialNetworkContainer: {
+    flexDirection: 'row'
   },
   purpleBox: {
     backgroundColor: 'purple',
--- 06-React-Native/react-native/topic02/topic02.js
+++ 06-React-Native/react-native/topic02/topic02.js
@@ -4,11 +4,51 @@ import styles from './styles.js';
 
 export default class Topic02 extends React.Component {
   render() {
+/*
+<View style={styles.container}>
+  <View style={[styles.box, styles.redBox]}></View>
+  <View style={[styles.box, styles.whiteBox]}></View>
+  <View style={[styles.box, styles.purpleBox]}></View>
+</View>
+*/
     return (
       <View style={styles.container}>
-        <View style={[styles.box, styles.redBox]}></View>
-        <View style={[styles.box, styles.whiteBox]}></View>
-        <View style={[styles.box, styles.purpleBox]}></View>
+        <View style={styles.article}>
+          <View style={styles.content}>
+            <Text style={styles.title}>Lorem ipsum dolor sit amet, consectetur adipiscing elit</Text>
+            <Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+              ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+              voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+              occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+              anim id est laborum.
+            </Text>
+            <View style={styles.socialNetworkContainer}>
+              <Image
+                source={require('./favicon.png')}
+                style={styles.socialNetworkImage}
+              />
+              <Image
+                source={require('./favicon.png')}
+                style={styles.socialNetworkImage}
+              />
+              <Image
+                source={require('./favicon.png')}
+                style={styles.socialNetworkImage}
+              />
+              <Image
+                source={require('./favicon.png')}
+                style={styles.socialNetworkImage}
+              />
+            </View>
+          </View>
+          <Image
+            source={require('./image.png')}
+            style={styles.articleImg}
+          />
+        </View>
       </View>
     );
   }

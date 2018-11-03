7f1708ae25b5ec58204f902c4b61469214ddde53
nicomanzato <nicomanzato@gmail.com>
2018-11-03 13:02:29 -0300

    topic01: 3. added

--- 06-React-Native/react-native/topic01/topic01.js
+++ 06-React-Native/react-native/topic01/topic01.js
@@ -1,34 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default class Topic01 extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.red}>
-          <Text>This view has a red background</Text>
-        </View>
-        <View style={styles.bordered}>
-          <Text>This view has a border</Text>
-        </View>
-        <View style={[styles.bordered, styles.red]}>
-          <Text>This view has a border and red background</Text>
-        </View>
+        <ScrollView style={styles.scrollView} horizontal>
+          <View style={styles.red}>
+            <Text>This view has a red background</Text>
+          </View>
+          <View style={styles.bordered}>
+            <Text>This view has a border</Text>
+          </View>
+          <View style={[styles.bordered, styles.red]}>
+            <Text>This view has a border and red background</Text>
+          </View>
+        </ScrollView>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: 'grey',
+    height: 300
+  },
   red: {
     backgroundColor: 'red',
+    flex: 2
   },
   bordered: {
     borderWidth: 1,
     borderColor: 'black',
   },
   container: {
-    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',

38d48c4ce02f69fba2aee8a675da079136eb23a9
nicomanzato <nicomanzato@gmail.com>
2018-11-03 12:13:18 -0300

    topic01: 1. added

--- 06-React-Native/react-native/topic01/topic01.js
+++ 06-React-Native/react-native/topic01/topic01.js
@@ -5,13 +5,28 @@ export default class Topic01 extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={styles.red}>
+          <Text>This view has a red background</Text>
+        </View>
+        <View style={styles.bordered}>
+          <Text>This view has a border</Text>
+        </View>
+        <View style={[styles.bordered, styles.red]}>
+          <Text>This view has a border and red background</Text>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  red: {
+    backgroundColor: 'red',
+  },
+  bordered: {
+    borderWidth: 1,
+    borderColor: 'black',
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
